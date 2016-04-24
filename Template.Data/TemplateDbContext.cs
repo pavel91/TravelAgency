e7e028a6d3fb0cfe4a8388de006136e8009cea78
pavel91 <pavel.shalev@abv.bg>
2016-04-24 14:13:05 +0300

    First Commit

---
+++ Template.Data/TemplateDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Models;
+using System.Data.Entity;
+using Template.Data.Migrations;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Template.Data
+{
+    public class TemplateDbContext : IdentityDbContext<ApplicationUser> , ITemplateDbContext
+    {
+        public TemplateDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TemplateDbContext, Configuration>());
+        }
+
+        public static TemplateDbContext Create()
+        {
+            return new TemplateDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Package>()
+                .HasMany(p => p.Participants)
+                .WithMany(u => u.Packages)
+                .Map(m =>
+                {
+                    m.ToTable("PackageUsers");
+                    m.MapLeftKey("PackageId");
+                    m.MapRightKey("ParticipantId");
+                });
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(au => au.Friends)
+                .WithMany()
+                .Map(m =>
+                {
+                    m.ToTable("UserFriends");
+                    m.MapLeftKey("UserId");
+                    m.MapRightKey("FriendId");
+                });
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public IDbSet<Comment> Comments { get; set; }
+        public IDbSet<Destination> Destinations { get; set; }
+        public IDbSet<Event> Events { get; set; }
+        public IDbSet<Extra> Extras { get; set; }
+        public IDbSet<Like> Likes { get; set; }
+        public IDbSet<Location> Locations { get; set; }
+        public IDbSet<Message> Messages { get; set; }
+        public IDbSet<Order> Orders { get; set; }
+        public IDbSet<Package> Packages { get; set; }
+        public IDbSet<Photo> Photos { get; set; }
+        public IDbSet<TourGuide> TourGuides { get; set; }
+        public IDbSet<Video> Videos { get; set; }
+    }
+}
---
+++ Template.Models/ApplicationUser.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private ICollection<ApplicationUser> friends;
+        //do we need it
+        private ICollection<Package> packages;
+        private ICollection<Comment> comments;
+        //private ICollection<Destination> destinations;
+
+        public ApplicationUser()
+        {
+            this.friends = new List<ApplicationUser>();
+            this.packages = new List<Package>();
+            this.comments = new List<Comment>();
+            //this.destinations = new List<Destination>();
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public Photo ProfilePic { get; set; }
+
+        public virtual ICollection<ApplicationUser> Friends
+        {
+            get { return this.friends; }
+            set { this.friends = value; }
+        }
+
+        public virtual ICollection<Package> Packages
+        {
+            get { return this.packages; }
+            set { this.packages = value; }
+        }
+
+        public virtual ICollection<Comment> Comments
+        {
+            get { return this.comments; }
+            set { this.comments = value; }
+        }
+
+        //public virtual ICollection<Destination> Destinations
+        //{
+        //    get { return this.destinations; }
+        //    set { this.destinations = value; }
+        //}
+    }
+}
